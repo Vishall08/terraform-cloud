9bccb41e456112ae791cb2ba74f8a32d0ce18433
Vishal Dhanokar <130133167+Vishall08@users.noreply.github.com>
2025-08-25 11:20:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,12 +10,12 @@ terraform {
  
 # Configure the AWS Provider
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-2"
 access_key = var.access_key
 secret_key = var.secret_key
 }
 resource "aws_instance" "myinstance" {
-  ami           = "ami-0e35ddab05955cf57"
+  ami           = "ami-0b016c703b95ecbe4"
   instance_type = "t2.micro"
   count = 2
   tags = {
