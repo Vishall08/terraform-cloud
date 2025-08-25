bfb3c075e8644d078b1e350e26a4db5b55a95792
Vishal Dhanokar <130133167+Vishall08@users.noreply.github.com>
2025-08-25 11:03:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,13 @@
-
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+provider "aws" {
+    region = var.my-region
+    access_key = var.access_key
+    secret_key = var.secret_key  
+}
