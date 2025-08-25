b3e98c26aeed0155f07a9d35707ddf51d092bbfc
Vishal Dhanokar <130133167+Vishall08@users.noreply.github.com>
2025-08-25 11:07:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,24 @@
 terraform {
+  required_version = "~> 1.1"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.0"
+      version = "~> 5.0"
     }
   }
 }
+ 
+# Configure the AWS Provider
 provider "aws" {
-    region = var.my-region
-    access_key = var.access_key
-    secret_key = var.secret_key  
+  region = "ap-south-1"
+access_key = var.access_key
+secret_key = var.secret_key
+}
+resource "aws_instance" "myinstance" {
+  ami           = "ami-0e35ddab05955cf57"
+  instance_type = "t2.micro"
+  count = 2
+  tags = {
+    Name = "myinstance ${count.index+1}"
+}
 }
